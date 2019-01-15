fa6b5b3132c793dac7f67289fa36437b23c623ca
Lukap992 <Luka.Persj992@gmail.com>
2019-01-15 19:04:52 +0100

    Uradjena klasa Roba

--- src/main/java/model/Roba.java
+++ src/main/java/model/Roba.java
@@ -24,13 +24,57 @@ public class Roba {
     @DatabaseField(columnName = POLJE_OPIS, canBeNull = false)
     private String opis;
 
-    @DatabaseField(columnName = POLJE_TEZINA, canBeNull = fasle)
+    @DatabaseField(columnName = POLJE_TEZINA, canBeNull = false)
     private double tezina;
 
     public Roba(){
 
     }
+    public Roba(int id, String naziv, double tezina){
+        this.id = id;
+        this.naziv = naziv;
+        this.tezina = tezina;
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public double getTezina() {
+        return tezina;
+    }
+
+    public void setTezina(double tezina) {
+        this.tezina = tezina;
+    }
+
+    @Override
+    public String toString() {
+        return "Roba{" +
+                "id=" + id +
+                ", naziv='" + naziv + '\'' +
+                ", opis='" + opis + '\'' +
+                ", tezina=" + tezina +
+                '}';
+    }
 }
